52e9cbd961922f917cf9119716ac31f56d58dbf4
Hagop Taminian <hagop.taminian@gmail.com>
2023-12-19 16:15:57 +0300

    feat: allow carbon dioxide molecule to be rotated
    closes #37

--- src/components/canvas/molecules/atoms/Carbon.tsx
+++ src/components/canvas/molecules/atoms/Carbon.tsx
@@ -2,13 +2,20 @@ import { Circle } from 'react-konva';
 
 import { CARBON_FILL, CARBON_RADIUS } from '@/constants/canvas';
 
+const defaultProps = {
+  x: 0,
+  y: 0,
+};
+
 interface Props {
-  x: number;
-  y: number;
+  x?: number;
+  y?: number;
 }
 
 const Carbon = ({ x, y }: Props): JSX.Element => (
   <Circle fill={CARBON_FILL} radius={CARBON_RADIUS} x={x} y={y} />
 );
+
+Carbon.defaultProps = defaultProps;
 
 export default Carbon;
