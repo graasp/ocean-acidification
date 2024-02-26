13be848e97712024bd7cd1d02ac5a5e310abeaf3
Hagop Taminian <hagop.taminian@gmail.com>
2024-02-26 12:53:33 +0300

    feat: allow co2 motion to be reversed
    closes #78

--- src/components/canvas/motion/CarbonDioxideMigration.tsx
+++ src/components/canvas/motion/CarbonDioxideMigration.tsx
@@ -10,21 +10,25 @@ import CarbonDioxide from '../molecules/CarbonDioxide';
 interface Props {
   beginsAfter: number;
   molecules: Migration;
+  reverse: boolean;
 }
 
 const CarbonDioxideMigration = ({
   beginsAfter,
   molecules,
+  reverse,
 }: Props): JSX.Element => {
   const { state } = useContext(AppSettingsContext);
   const { intervalCount, dimensions } = state;
   const { width, height } = dimensions;
   const { co2 } = molecules;
   const netIntervals = intervalCount - beginsAfter;
+  const reversedCo2 = { begins: { ...co2.ends }, ends: { ...co2.begins } };
+  const activeCo2 = reverse ? reversedCo2 : co2;
 
-  const currentX = computePosition(co2, X, netIntervals);
-  const currentY = computePosition(co2, Y, netIntervals);
-  const currentRotation = computePosition(co2, ROTATION, netIntervals);
+  const currentX = computePosition(activeCo2, X, netIntervals);
+  const currentY = computePosition(activeCo2, Y, netIntervals);
+  const currentRotation = computePosition(activeCo2, ROTATION, netIntervals);
 
   return (
     <CarbonDioxide
