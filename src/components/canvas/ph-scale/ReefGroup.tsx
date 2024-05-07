647555d945223d1b4337c2ff865207f2df26db7d
Hagop Taminian <hagop.taminian@gmail.com>
2024-05-07 19:29:17 +0300

    feat: move shell and crab in front of reef
    closes #133

--- src/components/canvas/ph-scale/ReefGroup.tsx
+++ src/components/canvas/ph-scale/ReefGroup.tsx
@@ -4,12 +4,14 @@ import { REEF_GROUP } from '@/constants/canvas';
 
 import Reef from './Reef';
 import ReefHoles from './ReefHoles';
+import Organisms from './organisms/Organisms';
 
 const ReefGroup = (): JSX.Element => (
   <Group>
     {REEF_GROUP.map(({ x, y, rotation }, index) => (
       <Reef x={x} y={y} rotation={rotation} key={index} />
     ))}
+    <Organisms />
     <ReefHoles />
   </Group>
 );
