9295dbf0b1db13204fe35b5c29cf773a668f8a16
Hagop Taminian <hagop.taminian@gmail.com>
2024-05-21 13:53:04 +0300

    feat: add ppm under year
    closes #144

--- src/components/side-menu/continuous-mode/Period.tsx
+++ src/components/side-menu/continuous-mode/Period.tsx
@@ -39,6 +39,12 @@ const radioText = {
   fontSize: '0.8em',
 };
 
+const extraLabelText = {
+  color: 'grey',
+};
+
+const radioButtonContainer = { display: 'flex', flexDirection: 'column' };
+
 const Period = (): JSX.Element => {
   const { state, dispatch } = useContext(AppSettingsContext);
   const { isPlaying, sliderCarbonDioxide, equilibriumCarbonDioxide } = state;
@@ -69,14 +75,18 @@ const Period = (): JSX.Element => {
         value={atEquilibrium && currentYear}
         onChange={onChange}
       >
-        {PERIODS.map(({ year }) => (
-          <FormControlLabel
-            key={year}
-            value={year}
-            control={<Radio color="primary" size="small" sx={radioButton} />}
-            label={<Typography sx={radioText}>{year}</Typography>}
-            disabled={isPlaying}
-          />
+        {PERIODS.map(({ year, co2 }) => (
+          <Box key={year} sx={radioButtonContainer}>
+            <FormControlLabel
+              value={year}
+              control={<Radio color="primary" size="small" sx={radioButton} />}
+              label={<Typography sx={radioText}>{year}</Typography>}
+              disabled={isPlaying}
+            />
+            <Typography variant="caption" sx={extraLabelText}>
+              ({co2} ppm)
+            </Typography>
+          </Box>
         ))}
       </RadioGroup>
     </Box>
